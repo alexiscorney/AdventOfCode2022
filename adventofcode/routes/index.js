450b3cda77ffbcd99601122a179dd2822e380a07
alexiscorney <adcorney@googlemmail.com>
2022-12-03 20:16:30 +0000

    add images, arrange days in a table, custom font

--- adventofcode/routes/index.js
+++ adventofcode/routes/index.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const { createPath } = require('../src/utils/pathHelper')
+const { createPath } = require('../src/utils/createPath')
+const { getImage } = require('../src/utils/getImage')
 const { controllers } = require('./days_list');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const days = []
   for(const d of Object.keys(controllers)) {
-    days.push({dayNumber: d, path: createPath(d)})
+    days.push({dayNumber: d, path: createPath(d), img: getImage(d)})
   }
   res.render('index', 
     { 
