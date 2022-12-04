da18203f57c3aab24ead38b3edf14d8b1a0f48b6
alexiscorney <adcorney@googlemmail.com>
2022-12-04 17:50:13 +0000

    day4 part 1 solved

--- adventofcode/src/controllers/day4.js
+++ adventofcode/src/controllers/day4.js
@@ -3,8 +3,20 @@ const { getInputPath } = require('../utils/pathHelper');
 
 const day4 = ((req, res) => {
     const arr = fileToArray(getInputPath('day4.txt'));
-    
-    const part1 = 0;
+    var pairs = []; 
+    arr.forEach((x,i) => {
+        pair = x.split(','); 
+        ranges = []
+        pair.forEach((y,i) => {
+            //console.log(y);
+            indexes = y.split('-');
+            ranges.push([...Array(parseInt(indexes[1]) - parseInt(indexes[0]) + 1).keys()].map(i => i + parseInt(indexes[0])));
+        })
+        ranges.sort(function(a,b) { return b.length - a.length });
+        pairs.push(ranges);
+    })
+    var overlapping_pairs = pairs.filter(pair => (pair[1].every(val => pair[0].includes(val))));
+    const part1 = overlapping_pairs.length;
     const part2 = 0;
     return { dayNumber: 4, part1: part1, part2:  part2};
 })
