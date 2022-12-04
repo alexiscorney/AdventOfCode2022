443f1a7c1bfd959ddfeacc215c4e69c2c5a0790f
alexiscorney <adcorney@googlemmail.com>
2022-12-04 18:44:30 +0000

    change createPath to createDaysRoute

--- adventofcode/routes/index.js
+++ adventofcode/routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-const { createPath } = require('../src/utils/pathHelper')
+const { createDaysRoute } = require('../src/utils/pathHelper')
 const { controllers } = require('./days_list');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const days = []
   for(const d of Object.keys(controllers)) {
-    days.push({dayNumber: d, path: createPath(d)})
+    days.push({dayNumber: d, path: createDaysRoute(d)})
   }
   res.render('index', 
     { 
