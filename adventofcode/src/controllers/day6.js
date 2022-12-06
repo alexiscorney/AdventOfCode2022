0774e60e9c7b444cc6474cf87ee1bc0d779a1d42
alexiscorney <adcorney@googlemmail.com>
2022-12-06 13:30:55 +0000

    day6 solved

--- adventofcode/src/controllers/day6.js
+++ adventofcode/src/controllers/day6.js
@@ -2,10 +2,23 @@ const { fileToArray } = require('../utils/readFile');
 const { getInputPath } = require('../utils/pathHelper');
 
 const day6 = ((req, res) => {
-    const arr = fileToArray(getInputPath('day6.txt'));
-    const part1 = 0;
-    const part2 = 0;
+    const arr = Array.from(fileToArray(getInputPath('day6.txt'))[0]);
+
+    const part1 = findMarker(arr, 4);
+    const part2 = findMarker(arr, 14);
     return { dayNumber: 6, part1: part1, part2:  part2};
 })
+
+function findMarker(arr, characters) {
+    acc = arr.slice(0, characters);
+    for(let i = 0; i < arr.length; i++) {
+        acc.shift();
+        acc.push(arr[i]);
+        s = new Set(acc);
+        if(s.size >= characters) {
+            return i+1;
+        }
+    }
+}
 
 module.exports = { day6 }
