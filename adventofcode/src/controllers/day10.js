be7ef6bfec6e3c990c3fe6cb27f33e87c32b11c0
alexiscorney <adcorney@googlemmail.com>
2022-12-11 14:58:17 +0000

    day 10 part 1 solved

--- adventofcode/src/controllers/day10.js
+++ adventofcode/src/controllers/day10.js
@@ -4,9 +4,46 @@ const { getInputPath } = require('../utils/pathHelper');
 const day10 = ((req, res) => {
     const arr = Array.from(fileToArray(getInputPath('day10.txt')));
 
-    const part1 = 0;
+    const program = execute(arr);
+    var signal_strength = calculateSignalStrenth(program);
+    for(i=0; i<program.length; i++) {
+        console.log(`i ${i} - X: ${program[i]}`);
+    }
+
+    const part1 = signal_strength;
     const part2 = 0;
     return { dayNumber: 10, part1: part1, part2:  part2};
 })
+
+function execute(arr) {
+    var X = 1;
+    var program = [];
+    program.push(X);
+    arr.forEach(row => {
+        c = row.split(' ');
+        command = c[0];
+        if(command === 'addx') {
+            val = parseInt(c[1]);
+            program.push(X);
+            program.push(X)
+            X+=val;
+            //program.push(X);
+        } else if(command === 'noop') {
+            program.push(X);
+        }
+    });
+    return program;
+}
+
+function calculateSignalStrenth(program) {
+    var i = 20;
+    var signal_strength = 0;
+    while(i<= 220) {
+        console.log(`i:${i} - ${signal_strength} + ${(program[i] * i)}`);
+        signal_strength += (program[i] * i);
+        i+=40;
+    }
+    return signal_strength;
+}
 
 module.exports = { day10 }
