8d4029592fc4fabb0b108950a15a53b9b0d80919
alexiscorney <adcorney@googlemmail.com>
2022-12-17 17:02:04 +0000

    day14 part 1 solved

--- adventofcode/src/utils/arrayHelper.js
+++ adventofcode/src/utils/arrayHelper.js
@@ -2,10 +2,17 @@ function create2DArray(width, height, fill) {
     return Array(height).fill().map(() => Array(width).fill(fill));
 }
 
+function log2DArray(grid, start_x, end_x, start_y, end_y) {
+    console.log(`0: ${Array.apply(0, Array(end_x - start_x + 1)).map((element, index) => index + start_x)}`)
+    for(let i = start_y; i < end_y; i++) {
+        console.log(`${i}: ${grid[i].slice(start_x, end_x)}`);
+    }
+}
+
 function chunkArray(arr, size) {
   return Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
     arr.slice(i * size, i * size + size)
   );
 }
 
-module.exports = { create2DArray, chunkArray }
+module.exports = { create2DArray, log2DArray, chunkArray }
