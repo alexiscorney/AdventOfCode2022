029c3ea63033b2e9d202b4f84d6cb22f055d8374
alexiscorney <adcorney@googlemmail.com>
2022-12-27 12:44:24 +0000

    commit what I have

--- adventofcode/src/controllers/day17.js
+++ adventofcode/src/controllers/day17.js
@@ -96,7 +96,7 @@ class Rock {
 
 }
 const day17 = ((req, res) => {
-    const arr = Array.from(fileToArray(getInputPath('day17.txt')))[0];
+    const arr = Array.from(fileToArray(getInputPath('day17test.txt')))[0];
 
     let covered_area = [];
     let index = 0;
@@ -112,32 +112,146 @@ const day17 = ((req, res) => {
     let top = -1; 
     let rock_i = 0;
 
-    for(let i=0; i < 2022; i++) {
+    // for(let i=0; i < 2022; i++) {
+    //     if(rock_i === 5) {
+    //         rock_i = 0;
+    //     }
+    //     [new_area, index, top] = spawnRock(rocks[rock_i], top, covered_area, arr, index);
+    //     rock_i+=1;
+    //     covered_area = covered_area.concat(new_area);
+    // }
+    // //plot(covered_area,top);
+    // console.log(`highest point = ${top + 1}`);
+
+    // for(let i=0; i < 10000; i++) {
+    //     if(rock_i === 5) {
+    //         rock_i = 0;
+    //     }
+    //     [new_area, index, top, finished] = spawnRock(rocks[rock_i], top, covered_area, arr, index);
+    //     rock_i+=1;
+    //     covered_area = covered_area.concat(new_area);
+    // }
+    //in between is 2597
+
+    let moves_no = arr.length;
+    console.log(`length of input = ${moves_no}`);
+    
+    let runs = Math.floor(1000000000000 / moves_no);
+    let remainder = 1000000000000 - (runs*moves_no);
+    
+    //one full run of input 
+     
+    let running = true;
+    let one_run = 0;
+    while(running) {
         if(rock_i === 5) {
             rock_i = 0;
         }
-        [new_area, index, top] = spawnRock(rocks[rock_i], top, covered_area, arr, index);
-        rock_i+=1;
+        [new_area, index, top, going] = spawnRock(rocks[rock_i], top, covered_area, arr, index);
+        if(going === false) {
+            console.log(`index run out`);
+            console.log(`height: ${top}`);
+            one_run = top;
+            running = false;
+        } 
+        rock_i++;
         covered_area = covered_area.concat(new_area);
-
+        
     }
-    //plot(covered_area,top);
-    console.log(`highest point = ${top + 1}`);
+    console.log(`one run: ${one_run}`);
+
+    //two full run of input 
+    index = 0; 
+    running = true;
+    let two_run = 0;
+    while(running) {
+        if(rock_i === 5) {
+            rock_i = 0;
+        }
+        [new_area, index, top, going] = spawnRock(rocks[rock_i], top, covered_area, arr, index);
+        if(going === false) {
+            console.log(`index run out`);
+            console.log(`height: ${top}`);
+            two_run = top;
+            running = false;
+        } 
+        rock_i++;
+        covered_area = covered_area.concat(new_area);
+    }
+
+    console.log(`two runs: ${two_run}`); 
+
+    index = 0; 
+    running = true;
+    let three_run = 0;
+    while(running) {
+        if(rock_i === 5) {
+            rock_i = 0;
+        }
+        [new_area, index, top, going] = spawnRock(rocks[rock_i], top, covered_area, arr, index);
+        if(going === false) {
+            console.log(`index run out`);
+            console.log(`height: ${top}`);
+            three_run = top;
+            running = false;
+        } 
+        rock_i++;
+        covered_area = covered_area.concat(new_area);
+    }
+
+    console.log(`three runs: ${three_run}`); 
+
+    console.log(`every run of input adds ${three_run - two_run} height`);
+
+
+
+    running = true;
+    index = 0;
+    let rest = 0;
+    while(running) {
+        if(rock_i === 5) {
+            rock_i = 0;
+        }
+        [new_area, index, top, going] = spawnRock(rocks[rock_i], top, covered_area, arr, index, remainder);
+        if(going === false) {
+            console.log(`index run out`);
+            console.log(`height: ${top}`);
+            rest = top;
+            running = false;
+        } 
+        rock_i++;
+        covered_area = covered_area.concat(new_area);
+    }
+    rest = rest - three_run; 
 
     
+    
+    console.log(`there are ${runs} full runs and ${remainder} left over in 1000000000000`);
+    console.log(`after one run ${one_run}`);
+    console.log(`after two runs ${two_run}`);
+    console.log(`after three runs ${three_run}`);
+    console.log(`after remainder ${rest}`);
 
+    let height_added = ((three_run - two_run - 1) * runs) + rest; 
+    //console.log(runs * 2597);
+
+    console.log(`total height ${height_added}`);
     const part1 = top + 1;
-    const part2 = 0;
+    const part2 = top + 1;
     return { dayNumber: 17, part1: part1, part2:  part2};
 })
 
-function spawnRock(rock, top, covered_area, moves, index) {
+function spawnRock(rock, top, covered_area, moves, index, run_until = null) {
     rock.setPosition(2, top+4);
     let falling = true;
     let lateral = true;
     while(falling) {
         if(lateral) {
-            if(index === moves.length) {
+            if(index === moves.length || index === run_until) {
+                console.log(`index run out`);
+                console.log(`height: ${top}`);
+                //plot(covered_area, top);
+                return [rock.generateArea(), index, top, false];;
                 index = 0;
             }
             let next_move = moves[index];
@@ -158,7 +272,7 @@ function spawnRock(rock, top, covered_area, moves, index) {
         }
     }
     top = Math.max(top, rock.getPosition()._y + rock.height -1);
-    return [rock.generateArea(), index, top];
+    return [rock.generateArea(), index, top, true];
 }
 
 function plot(area, max) {
